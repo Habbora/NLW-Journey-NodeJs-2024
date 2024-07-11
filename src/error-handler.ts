d63d5ba7500740aa17257140cab554ca67b93f44
habbora <alisson.queirois@gmail.com>
2024-07-11 17:57:23 -0300

    atualizacao erros

--- src/error-handler.ts
+++ src/error-handler.ts
@@ -1,9 +1,24 @@
 import type { FastifyInstance } from "fastify"
+import { ClientError } from "./errors/client-error"
+import { ZodError } from "zod"
 
 type FastifyClientErrorHandler = FastifyInstance['errorHandler']
 
-export const ClientErrorHandler: FastifyClientErrorHandler = (ClientError, request, response) => {
+export const ClientErrorHandler: FastifyClientErrorHandler = (error, request, response) => {
     console.log(ClientError)
+
+    if (error instanceof ZodError) {
+        return response.status(400).send({
+            message: 'Invalid Input',
+            errors: error.flatten().fieldErrors
+        })
+    }
+
+    if (error instanceof ClientError) {
+        return response.status(400).send({
+            message: error.message
+        })
+    }
 
     return response.status(500).send({ message: 'Internal Server ClientError'})
 }
