4ea74787542f7686d91488654c51fdfc9dea2b2a
habbora <alisson.queirois@gmail.com>
2024-07-11 17:41:02 -0300

    novas rotas

--- src/server.ts
+++ src/server.ts
@@ -6,12 +6,15 @@ import { confirmTrip } from './routes/confirm-trip';
 import { ConfirmParticipant } from './routes/confirm-participant';
 import { createActivity } from './routes/create-activity';
 import { getActivities } from './routes/get-activities';
-import { createLink } from './routes/creates-link';
+import { createLink } from './routes/create-link';
 import { getLinks } from './routes/get-links';
 import { getTrip } from './routes/get-trips';
 import { getParticipants } from './routes/get-participants';
 import { createInvites } from './routes/create-invite';
 import { env } from './env';
+import { updateTrip } from './routes/update-trip';
+import { getTripDetails } from './routes/get-trip-details';
+import { getParticipantDetails } from './routes/get-participant-details';
 
 const app = fastify()
 
@@ -24,15 +27,17 @@ app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrips)
-app.register(getTrip)
 app.register(confirmTrip)
-app.register(getParticipants)
 app.register(ConfirmParticipant)
 app.register(createActivity)
 app.register(getActivities)
 app.register(createLink)
 app.register(getLinks)
+app.register(getParticipants)
 app.register(createInvites)
+app.register(updateTrip)
+app.register(getTripDetails)
+app.register(getParticipantDetails)
 
 app.listen({port: env.PORT}).then(() => {
     console.log('Server running');
